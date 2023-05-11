e53cdb06c9f58b698e9b1ecc01100fb415d0e5ba
Tiffany Jaakmees <tjaakmee@tktk.ee>
2023-05-11 09:26:07 +0300

    add new files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    let url = 'https://api.themoviedb.org/3/movie/14?api_key=d6d38ef99bbabac0bedea74b69dc20fc';
+    axios.get(url)
+    .then(response => {
+        console.log(response.data.title);
+        let data = response.data;
+        let releaseDate = new Date(data.release_date).getFullYear();
+        let genres = '';
+
+        data.genres.forEach(genre => {
+            genres = genres + `${genre.name}, `;
+        });
+
+        
+        let genresUpdated = genres.slice(0, -2) + '.';
+        moviePoster = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+        console.log(genresUpdated);
+        let currentYear = new Date().getFullYear();
+        res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
+    });
+    
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running');
+});
